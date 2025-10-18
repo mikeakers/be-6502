bfc008fb3895d5d20a8c203c6222f86bf922645f
Mike Akers <mike.akers@gmail.com>
2025-10-18 17:24:15 -0400

    4-bit lcd before rewiring

--- helloworld-subroutines-checkbusy-4bit.s
+++ helloworld-subroutines-checkbusy-4bit.s
@@ -12,7 +12,23 @@ RS =  %00100000
 reset:
   ldx #$ff         ; Set stack pointer to top of the stack range 
   tsx
+  jsr init_lcd
 
+  ldx #0
+print_loop:
+  lda message,x
+  beq halt 
+  jsr send_char
+  inx
+  jmp print_loop
+
+halt:
+  stp
+
+message: .asciiz "  Hello, 4-bit                            mode! :D" 
+
+init_lcd:
+  pha
   lda #%11111111  ; set all pins on port B to output
   sta DDRB
   lda #%11100000  ; set top 3 pins on port A to output
@@ -21,39 +37,31 @@ reset:
   lda #%00100000  ; set 4 bit operation
   jsr send_instruction
 
-  lda #%00100000  ; 4 bit operation, 2 line display part 1
-  jsr send_instruction
-  lda #%10000000  ; 4 bit operation, 1 line display part 2
-  jsr send_instruction
+  lda #%00101000  ; 4 bit operation, 2 line display
+  jsr send_instruction4
 
-  lda #%00000000  ; turn on display and cursor part 1
+  lda #%00001110  ; turn on display and cursor
+  jsr send_instruction4
+
+  lda #%00000110  ; set mode to increment and shift cursor
+  jsr send_instruction4
+
+  pla
+  rts
+
+send_instruction4:
+  pha
   jsr send_instruction
-  lda #%11100000  ; turn on display and curdor part 2
+  rol
+  rol
+  rol
+  rol
   jsr send_instruction
-
-  lda #%00000000  ; set mode to increment and shift cursor part 1
-  jsr send_instruction
-  lda #%01100000  ; set mode to increment and shift cursor part 2
-  jsr send_instruction
-
-
-  ; lda #%01001000  ; send high half of H
-  ; jsr send_char
-
-  ldx #0
-print_loop:
-  lda message,x
-  beq loop
-  jsr send_char
-  inx
-  jmp print_loop
-  
-loop:
-  jmp loop
-
-message: .asciiz "  Hello, 4-bit                            mode! :D" 
+  pla
+  rts
 
 send_instruction:
+  pha
   jsr lcd_wait
   sta PORTB
   lda #0          ; bits for setting control lines to set display type
@@ -62,10 +70,11 @@ send_instruction:
   sta PORTA
   lda #0          ; clear control bits
   sta PORTA
+  pla
   rts
 
 send_char:
-  sta $00
+  pha             ; push char to print onto stack
   jsr lcd_wait
   sta PORTB
   lda #RS          ; clear control bits
@@ -75,8 +84,9 @@ send_char:
   lda #RS          ; clear control bits
   sta PORTA
 
-  lda $00
-  rol
+  pla             ; pop char to print from stack
+  pha             ; push char back to stack so it can be restored before rts
+  rol             ; shift left 4 bits
   rol
   rol
   rol
@@ -89,6 +99,7 @@ send_char:
   sta PORTA
   lda #RS          ; clear control bits
   sta PORTA
+  pla
   rts
 
 lcd_wait:
